d3aa7aa1399fbb2b8c17c75f5fca6f73c239796d
Sincer Barkar <sincer_barkar@yahoo.co.in>
2020-09-02 15:44:19 +0530

    Initial Commit

---
+++ src/main/java/com/brecnis/learn/jenkins/App.java
@@ -0,0 +1,13 @@
+package com.brecnis.learn.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
